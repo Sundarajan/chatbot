3a94ec2ac20c8a51038665891a867ebeb060a7ea
Sundarajan <contactsundar.t@gmail.com>
2019-03-19 15:13:34 +0530

    fix

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
+
+const bodyParser = require("body-parser");
     
 app.use(morgan('combined'));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(bodyParser.json());
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';	
@@ -12,6 +21,22 @@ app.get('/', function (req, res) {
      // res.writeHead(200, {'Content-Type': 'text/plain'});
 	  res.end('Greetings');
 });
+app.post("/", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "test-sample"
+  });
+});
+
+
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
