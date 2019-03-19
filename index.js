3c4585937e25a6bc18454bece017d0e70e31cd61
Sundarajan <contactsundar.t@gmail.com>
2019-03-19 13:29:50 +0530

    tes

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ var express = require('express'),
 app.use(morgan('combined'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';	
     
 
 app.get('/', function (req, res) {
-      res.writeHead(200, { Content-Type : "text-plain" });
+     // res.writeHead(200, {'Content-Type': 'text/plain'});
 	  res.end('Greetings');
 });
-
-
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
