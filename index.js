bc2d4a5dd8de5d56cc7c99f90447680b764494fe
Sundarajan <contactsundar.t@gmail.com>
2019-03-19 13:22:59 +0530

    fix

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
     
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
     
 
 app.get('/', function (req, res) {
